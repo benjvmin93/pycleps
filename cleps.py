d2403049b53b99acfcedf7de0801ac168f0c951c
benjvmin93 <benjamin.guichard@inria.fr>
2025-03-03 11:05:41 +0100

    fix exec_cmd + handle exception in clone repo in case the repo already exists

--- cleps.py
+++ cleps.py
@@ -21,23 +21,27 @@ class ClepsSSHWrapper:
         self.username = username
 
     def exec_cmd(self, cmd: str) -> None:
-        _, stdout, stderr = self.exec_cmd(cmd)
-        if stderr:
-            raise Exception(stderr)
-        print(stdout)
+        _, stdout, stderr = self.client.exec_command(cmd)
+
+        err = stderr.read().decode()
+        if err:
+            raise Exception(err)
+
+        out = stdout.read().decode()
+        print(out)
 
     def clone_repo(self, repo_addr: str | Path, dst_dir: Path = "~/") -> None:
         """Clone repository from github or transfer it from your machine to the cluster."""
         if repo_addr.startswith("git@github.com") or repo_addr.startswith("https://"):  # Clone repo from github
             print("Cloning repository from Github...")
             repo_name = repo_addr.split("/")[-1].replace(".git", "")
-            dst_path = dst_dir / repo_name
-            print(f"git clone {repo_addr} {dst_path}")
-            cmd = f"git clone {repo_addr} {dst_path}"
-            self.exec_cmd(cmd)
-            print(f"Repository cloned as {dst_path}")
+            cmd = f"git clone {repo_addr} {dst_dir}"
+            try:
+                self.exec_cmd(cmd)
+                print(f"Repository cloned as {dst_dir}")
+            except Exception as e:  # Eventually, the repo already exists in the cluster machine
+                print(e)
         else:   # Transfer the repo from local machine
-            print(f"Copying {repo_addr} to {dst_dir}")
             self.exec_cmd(f"mkdir -p {dst_dir}")
             with SCPClient(self.client.get_transport()) as scp:
                 scp.put(Path(repo_addr), recursive=True, remote_path=dst_dir)
@@ -45,12 +49,6 @@ class ClepsSSHWrapper:
 
     def send_job(self, run_cmd: str, working_dir: Path, slurm_options: SlurmOptions, sbatch_options: SbatchHeader, env_cmd: str | None = None) -> None:
         """Schedule a job that will run your script on the cluster."""
-        
-        _, stdout, stderr = self.exec_cmd('ls ~/')
-        job_submission_err = stderr.read().decode()
-        job_submission_out = stdout.read().decode()
-        print(job_submission_out)
-
         slurm_script_path = working_dir / "slurm_job.sbatch"
         slurm_directives = slurm_options.to_slurm_directives()
         slurm_script = f"""#!/bin/bash
@@ -66,6 +64,5 @@ source ~/.bashrc
             scp.putfo(
                 io.StringIO(slurm_script), slurm_script_path
             )
-
         cmd = f"sbatch {sbatch_options} {slurm_script_path}"
         self.exec_cmd(cmd)
