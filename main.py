128f5feefd66450e9d20b05e09d51cb2a5e2e0cb
benjvmin93 <benjamin.guichard@inria.fr>
2025-03-03 11:06:31 +0100

    remove dir creation as it is already done in the clone repo method + small fixes

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     parser = argparse.ArgumentParser(description="Cleps Job Submission Tool")
     parser.add_argument("--username", help="Your Cleps username")
     parser.add_argument("--repo", required=True, help="Repository address (e.g., git@github.com:user/repo.git)")
-    parser.add_argument("--wd", default="~/", help="Working directory where the git repo will be copied (default: '.')")
+    parser.add_argument("--wd", default=".", help="Working directory where the git repo will be copied (default: '.')")
     parser.add_argument("--script", required=True, help="The command that will be used to run your script")
     parser.add_argument("--env", required=False, help="The command that will be used to set your jobs in the right environment. The environment should already be created within the cluster.")
     
@@ -26,22 +26,23 @@ def main():
         repo_name = repo_addr.split("/")[-1].replace(".git", "")
 
     working_dir = Path(working_dir)
-    working_dir.mkdir(exist_ok=True)
+    # working_dir.mkdir(exist_ok=True)
     repo_path = working_dir / repo_name
 
     # Initialize ClepsSSHWrapper
     client = ClepsSSHWrapper("cleps.inria.fr", username=username)
+    client.exec_cmd(f"mkdir -p {working_dir}")
 
     try:
         # Clone the repository
-        client.clone_repo(repo_addr=repo_addr, dst_dir=working_dir)
+        client.clone_repo(repo_addr=repo_addr, dst_dir=repo_path)
 
         # Define Slurm options
         slurm_options = SlurmOptions(
             job_name=f"{repo_name}-job",
             output=f"{working_dir}/outputs"
         )
-        sbatch_options = SbatchHeader(account=username)
+        sbatch_options = SbatchHeader()
 
         client.send_job(
             run_cmd=script,
