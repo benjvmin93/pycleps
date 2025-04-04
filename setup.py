d1d1b3e2cfe900f407c9db5f90a93caa48fe15ee
benjvmin93 <benjamin.guichard@inria.fr>
2025-04-04 15:58:16 +0200

    using typer for argument

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     install_requires=requirements,
     entry_points={
         "console_scripts": [
-            "pycleps=pycleps.main:run",
+            "pycleps=pycleps.main:app",
         ],
     },
 )
